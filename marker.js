4a6186a1643d5bd7a22b1660411b17740cdd7905
Tom Ootes <tomootes@gmail.com>
2015-03-06 11:11:13 +0100

    Added conneting lines

--- marker.js
+++ marker.js
@@ -1,48 +1,31 @@
-function Tsv(path){
-  this.path = path;
+function Marker(index, color, x, y, z, radius){
+    this.index = index;
+    this.index = color;
+    this.index = x;
+    this.index = y;
+    this.index = z;
+    this.index = radius;
 
-  this.getFile = function(){
-    var xmlHttp = null;
-    xmlHttp = new XMLHttpRequest();
-    xmlHttp.open( "GET", this.path, false );
-    xmlHttp.send( null );
-    return xmlHttp.responseText;
-  };
 
-  this.toArray = function(s, first_row){
-    var coordinates = [];
-    coordinates[0] = [];
-    var char_index = 0;
-    var current_line = 0;
-    var current_coordinates = [];
+    var geometry = new THREE.SphereGeometry( radius, 32, 32 );
+    var material = new THREE.MeshPhongMaterial( {color: color} );
+    var marker = new THREE.Mesh( geometry, material );
+    marker.position.set(x,y,z);
+    marker.receiveShadow = true;
+    marker.castShadow = true;
+    marker.selected = false;
+    marker.index = index;
+    return marker;
 
-    for(var i = 0; i < s.length; i++){
-      if(s[i] == '\t'){
-        var sub = s.substr(char_index, (i - char_index));
-        sub = parseFloat(sub);
-        current_coordinates.push(sub);
-        char_index = i;
-        // Check how much coordinates are in the array, if 3 push to line array
-        if(current_coordinates.length == 3){
-          coordinates[current_line].push(current_coordinates); // Push coordinates to array
-          current_coordinates = []; // Empty array
-        }
-      }
-      if(s[i] == '\n'){
-        var sub = s.substr(char_index, (i - char_index));
-        sub = parseFloat(sub);
-        current_coordinates.push(sub);
-        char_index = i;
-        // Check how much coordinates are in the array, if 3 push to line array
-        if(current_coordinates.length == 3){
-          coordinates[current_line].push(current_coordinates); // Push coordinates to array
-          current_coordinates = []; // Empty array
-        }
-        current_line = current_line + 1;
-        coordinates[current_line] = [];
-      }
-    }  
-  return coordinates;
-  } 
-  
+    this.update = function{
+        var geometry = new THREE.SphereGeometry( radius, 32, 32 );
+        var material = new THREE.MeshPhongMaterial( {color: color} );
+        var marker = new THREE.Mesh( geometry, material );
+        marker.position.set(x,y,z);
+        marker.receiveShadow = true;
+        marker.castShadow = true;
+        marker.selected = false;
+        marker.index = index;
+        return marker;
+    }
 }
--- tsv.js
+++
@@ -1,6 +0,0 @@
-function tsv() {
-  // constructor
-  (function() {
-    
-  })();
-}
